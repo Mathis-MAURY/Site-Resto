b1f03c76c80125e29c9cb36935dff8188c2c6fb3
ethanmirbeau <ethan.mirbeauriey@limayrac.fr>
2024-09-26 11:45:31 +0200

    salut

--- Site-Restaurant/index.php
+++ Site-Restaurant/index.php
@@ -7,5 +7,4 @@
   <h1>Bienvenue sur l'AppResto !</h1>
   <p>Voici notre page d'accueil statique pour le moment, elle sera dynamique plus tard.</p>
 </section>
-
 <?php include 'fonctions/footer.php'; ?>
