497ff271874c2be1bef6a33c13b1d30af13553b0
MAURY Mathis <mathismaury31@gmail.com>
2024-10-03 11:14:15 +0200

    page connexion probleme redirigement vers commander résolu

--- Site-Restaurant/commander.php
+++ Site-Restaurant/commander.php
@@ -1,52 +1,167 @@
 <?php
-    $pageTitle= "Commander";
-    include 'fonctions/header.php';
-?>
-    <link rel="stylesheet" href="css/commander.css">
-    <h1>MENU</h1>
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 
-    <form action="payer.php" method="post">
-    <ul>
-        <li>
-            <span>Pizza Bosca (17€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="0">
-        </li>
-        <li>
-            <span>Pizza Full Formaggi (15€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="0">
-        </li>
-        <li>
-            <span>Pizza Basta Cosi (14€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="2">
-        </li>
-        <li>
-            <span>Katsu viande et aubergines (14,90€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="1">
-        </li>
-        <li>
-            <span>Katsu poisson et aubergines (12,90€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="0">
-        </li>
-        <li>
-            <span>Coxinhas boeuf (9,90€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="1">
-        </li>
-        <li>
-            <span>Coxinhas vegan (7,90€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="1">
-        </li>
-        <li>
-            <span>Coxinhas fromage (7,90€)</span>
-            <input type="number" min="0" max="100" step="1" placeholder="0">
-        </li>
-    </ul>
-    <label class="radio-label">
-        <input type="radio" name="commande" value="surplace">
-        <span class="custom-radio"></span> Sur place<br><br>
-        <input type="radio" name="commande" value="emporter">
-        <span class="custom-radio"></span> À emporter<br><br>
-        <button type="submit">Commander</button>&nbsp;&nbsp;&nbsp;
-        <a href="index.php">Annuler</a>
-    </label>
-    </form> 
-    <?php include 'fonctions/footer.php'; ?>
+if (!isset($_SESSION["user"])) {
+    header("Location: connexion.php");
+    die();
+}
+
+function autoloader($className)
+{
+    include "fonctions/$className.php";
+}
+
+spl_autoload_register("autoloader");
+
+$connexionBDD = new ConnexionBDD();
+$plats = $connexionBDD->prepareAndFetchAll(
+    "SELECT * FROM produit"
+);
+$panier = json_decode($_COOKIE["panier"] ?? "[]", true);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Fée - Restaurant de mafé</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <img src="assets/images/log.png" class="logo">
+    </header>
+    <aside id="profile">
+        <div class="head">
+            <div class="left">
+                Barre d'outils
+            </div>
+            <div class="right">
+                <i class="fa-solid fa-chevron-left"></i>
+            </div>
+        </div>
+
+        <div class="controls">
+            <a href="deconnexion.php" class="logout">
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+                Déconnexion
+            </a>
+        </div>
+    </aside>
+    <aside id="cart">
+        <div class="head">
+            <p class="title">Panier</p>
+        </div>
+        <div class="content">
+
+            <?php
+            $montantTotalPanier = 0;
+
+            $panier = json_decode($_COOKIE["panier"] ?? "[]", true);
+
+            foreach ($panier as $item) {
+                $idProduit = $item["id_produit"];
+                $quantity = $item["qty"];
+
+                $produit = recupererInfosProduit($idProduit, $plats);
+                if ($produit) {
+                    $libelle = $produit["libelle"];
+                    $prix = number_format($produit["prix_ht"] * $quantity, 2);
+                    $montantTotalPanier += $produit["prix_ht"] * $quantity;
+
+                    echo "
+                        <div class=\"item\">
+                        <input type=\"text\" value=\"$quantity x\" disabled>
+                        <p class=\"name\">$libelle</p>
+                        <p class=\"price\">{$prix}€</p>
+
+                        <a class=\"delete\" data-item-id=\"$idProduit\" onclick=\"supprimerDuPanier(this)\" href=\"javascript:void(0);\">
+                            <i style=\"text-align: center;\" class=\"fa-solid fa-x\"></i>
+                        </a>
+                    </div>
+                        ";
+                }
+            }
+
+            function recupererInfosProduit(int $idProduit, array $produits): array
+            {
+
+                foreach ($produits as $produit) {
+                    if ($produit["id_produit"] == $idProduit)
+                        return $produit;
+                }
+
+                return [];
+            }
+                ?>
+
+        </div>
+        <div class="bottom">
+            <button onclick="showPopup();">Payer !
+                <span class="price">
+                    <?= "(" . number_format($montantTotalPanier, 2) . "€ H.T)" ?>
+                </span>
+            </button>
+        </div>
+    </aside>
+
+
+    <div class="container">
+        <?php
+        $i = 0;
+
+        foreach ($plats as $plat) {
+            $i++;
+
+            $id = $plat["id_produit"];
+            $libelle = $plat["libelle"];
+            $prix = $plat["prix_ht"];
+            $imageUrl = $plat["imageUrl"];
+            echo "
+                <div class=\"item\" data-item-id=\"$id\">
+                <img src=\"$imageUrl\" class=\"preview\">
+                <div class=\"content\">
+                        <p class=\"name\">$libelle</p>
+                        <p class=\"description\"></p>
+
+                        <p class=\"bottom\">
+                            <button class=\"ajoutPanier\" data-item-id=\"$id\" onclick=\"ajouterAuPanier(this)\">
+                                <i class=\"fa-solid fa-cart-shopping\"></i>
+                                <span>
+                                    Ajout au panier <span class=\"price\">" . number_format($prix, 2) . "</span> 
+                                </span>                        
+                            </button>
+                        </p>
+                    </div>
+                </div>
+            ";
+        }
+        ?>
+    </div>
+
+    <div id="popup-type-conso" style="display: none;">
+        <div class="wrapper">
+            <p class="title">Choisissez un moyen de livraison</p>
+
+            <div class="mode" data-type-conso="1">
+                <img src="https://img.icons8.com/?size=256&id=543&format=png">
+                Sur place
+            </div>
+            <div class="mode" data-type-conso="2">
+                <i class="fas fa-shipping-fast"></i>
+                A emporter
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/profileModule.js"></script>
+    <script src="assets/js/panier.js"></script>
+</body>
+
+</html>
--- Site-Restaurant/sql-exports/db_resto.sql
+++ Site-Restaurant/sql-exports/db_resto.sql
@@ -3,10 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 10 sep. 2023 à 16:37
+-- Généré le : jeu. 30 nov. 2023 à 17:07
 -- Version du serveur : 10.4.28-MariaDB
--- Version de PHP : 8.1.17
-  
+-- Version de PHP : 8.2.4
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -29,7 +29,6 @@ USE `db_restoweb`;
 -- Structure de la table `commande`
 --
 
-DROP TABLE IF EXISTS `commande`;
 CREATE TABLE `commande` (
   `id_commande` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
@@ -39,13 +38,21 @@ CREATE TABLE `commande` (
   `type_conso` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id_commande`, `id_user`, `id_etat`, `date`, `total_commande`, `type_conso`) VALUES
+(6, 8, 1, '2023-11-30 16:58:27', 35.87, 1),
+(7, 8, 1, '2023-11-30 16:59:36', 37.40, 2),
+(8, 8, 1, '2023-11-30 17:02:05', 35.87, 1);
+
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `ligne`
 --
 
-DROP TABLE IF EXISTS `ligne`;
 CREATE TABLE `ligne` (
   `id_ligne` int(11) NOT NULL,
   `id_commande` int(11) NOT NULL,
@@ -55,9 +62,23 @@ CREATE TABLE `ligne` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
+-- Déchargement des données de la table `ligne`
+--
+
+INSERT INTO `ligne` (`id_ligne`, `id_commande`, `id_produit`, `qte`, `total_ligne_ht`) VALUES
+(17, 6, 3, 1, 12.50),
+(18, 6, 2, 1, 14.00),
+(19, 6, 5, 1, 7.50),
+(20, 7, 3, 1, 12.50),
+(21, 7, 2, 1, 14.00),
+(22, 7, 5, 1, 7.50),
+(23, 8, 3, 1, 12.50),
+(24, 8, 2, 1, 14.00),
+(25, 8, 5, 1, 7.50);
+
+--
 -- Déclencheurs `ligne`
 --
-DROP TRIGGER IF EXISTS `after_ligne_insert`;
 DELIMITER $$
 CREATE TRIGGER `after_ligne_insert` AFTER INSERT ON `ligne` FOR EACH ROW BEGIN
     set @total_commande = 0;
@@ -77,7 +98,6 @@ CREATE TRIGGER `after_ligne_insert` AFTER INSERT ON `ligne` FOR EACH ROW BEGIN
   END
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `after_ligne_update`;
 DELIMITER $$
 CREATE TRIGGER `after_ligne_update` AFTER UPDATE ON `ligne` FOR EACH ROW BEGIN
     set @total_commande = 0;
@@ -97,7 +117,6 @@ CREATE TRIGGER `after_ligne_update` AFTER UPDATE ON `ligne` FOR EACH ROW BEGIN
   END
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `before_ligne_insert`;
 DELIMITER $$
 CREATE TRIGGER `before_ligne_insert` BEFORE INSERT ON `ligne` FOR EACH ROW BEGIN
     set @prix_ht = 0;
@@ -108,7 +127,6 @@ CREATE TRIGGER `before_ligne_insert` BEFORE INSERT ON `ligne` FOR EACH ROW BEGIN
   END
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `before_ligne_update`;
 DELIMITER $$
 CREATE TRIGGER `before_ligne_update` BEFORE UPDATE ON `ligne` FOR EACH ROW BEGIN
     set @prix_ht = 0;
@@ -126,12 +144,28 @@ DELIMITER ;
 -- Structure de la table `produit`
 --
 
-DROP TABLE IF EXISTS `produit`;
 CREATE TABLE `produit` (
   `id_produit` int(11) NOT NULL,
   `libelle` varchar(255) NOT NULL,
-  `prix_ht` decimal(10,2) NOT NULL
+  `prix_ht` decimal(10,2) NOT NULL,
+  `imageUrl` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`id_produit`, `libelle`, `prix_ht`, `imageUrl`) VALUES
+(1, 'pizza Margherita', 12.50, 'https://img.passeportsante.net/1200x675/2022-09-23/shutterstock-2105210927.webp'),
+(2, 'pizza Chorizo', 14.00, 'https://www.galbani.fr/wp-content/uploads/2017/07/Image7.jpg'),
+(3, 'assiette de charcuterie', 12.50, 'https://www.passionculinaire.fr/wp-content/uploads/2021/09/charcuterie-conseils.jpg'),
+(4, 'assiette de fromages', 10.50, 'https://cache.marieclaire.fr/data/photo/w999_c17/cuisine/43/fromages1.jpg'),
+(5, 'hamburger viande', 7.50, 'https://www.la-viande.fr/sites/default/files/inline-images/hamburger.jpg'),
+(6, 'hamburger vegan', 9.00, 'https://violifefoods.com/wp-content/uploads/2020/12/vegan-spicy-burger-1920x850.jpg'),
+(7, 'hot dog', 5.00, 'https://www.lidl-recettes.fr/var/site/storage/images/_aliases/960x540/3/1/6/2/2592613-1-fre-FR/Prospectus-S41_Hot-dog-bacon.jpg'),
+(8, 'empanadas poulet', 11.00, 'https://www.enviedebienmanger.fr/sites/default/files/styles/img_orig/public/2020-12/68_1.png?itok=lfzsdW9K'),
+(9, 'empanadas thon', 9.00, 'https://www.seb.fr/medias/?context=bWFzdGVyfHJvb3R8NDQ4ODh8aW1hZ2UvanBlZ3xoM2YvaDI0LzE2NDgwMTYzMzk3NjYyLmpwZ3wzYTRjYWI3OTFkYjE5ZGFhODQ4MGZkMThiNDI3Njc3MTQ3MWE3OTkwYTc3MTYwYjc1YTI4ZGU5NjgyZTAzZGU4'),
+(10, 'portion de frites', 5.00, 'https://brigade-hocare.com/info/wp-content/uploads/2022/10/portion-de-frites.jpg');
 
 -- --------------------------------------------------------
 
@@ -139,13 +173,21 @@ CREATE TABLE `produit` (
 -- Structure de la table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `login`, `password`, `email`) VALUES
+(1, 'jef', '$2y$10$OPEElGC0F4PghGraYRFl7OmH57cnVWvApKYF/oWDRmEkj8PVhiwBu', 'jef@m2l.fr'),
+(8, 'test', '$2y$10$wgYsVsexeJHE0Bitxz2OXuzvyGZEhEZdYoR/cxS02SnRkkCmLrxxa', 'test@test.fr'),
+(10, '', '$2y$10$ZjmEtsUSjb4S4Z.OYC1XhOwDRoGd81LgxG0D/m4nNft5rh.HcAHTS', 'test2@test.fr');
 
 --
 -- Index pour les tables déchargées
@@ -187,25 +229,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `ligne`
 --
 ALTER TABLE `ligne`
-  MODIFY `id_ligne` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ligne` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `produit`
 --
 ALTER TABLE `produit`
-  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Contraintes pour les tables déchargées
