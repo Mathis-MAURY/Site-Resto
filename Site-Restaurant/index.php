556428f3f3c6547e6816ebf7883c7d6c04e9e1a7
sylvainfaccin <sylvain.faccin@limayrac.fr>
2024-10-10 11:58:19 +0200

    CSS MAJ

--- Site-Restaurant/index.php
+++ Site-Restaurant/index.php
@@ -3,12 +3,12 @@
   include 'fonctions/header.php';
 ?>
 
+<img src="images/FLASHMEAL.jpg" alt="LOGO" class="logo">
+
 <body>
 <section>
   <h1>Bienvenue sur <span style="color: #00A45B;">FlashMeal</span></h1>
 </section> 
-
-<img src="images/FLASHMEAL.jpg" alt="LOGO" class="logo">
 
 <?php include 'fonctions/footer.php'; ?>
 
