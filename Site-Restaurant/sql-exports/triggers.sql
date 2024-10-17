41fcfcd1305aa768faacc8a2e8397ca1fa35cdfd
sylvainfaccin <sylvain.faccin@limayrac.fr>
2024-10-17 08:52:42 +0200

    organisation triggerurs

--- Site-Restaurant/sql-exports/triggers.sql
+++ Site-Restaurant/sql-exports/triggers.sql
@@ -1,6 +1,4 @@
-DELIMITER $$
-
-CREATE TRIGGER `AFTER_INSERT_LIGNE` AFTER
+DELIMITER $$ CREATE TRIGGER `AFTER_INSERT_LIGNE` AFTER
     INSERT ON `LIGNE` FOR EACH ROW
 BEGIN
     DECLARE TOTALTTC DECIMAL(10, 2) DEFAULT 0;
@@ -27,6 +25,7 @@ BEGIN
             -- Mettre à jour le total de la commande
             UPDATE           COMMANDE SET TOTAL_COMMANDE = TOTALTTC WHERE COMMANDE.ID_COMMANDE = NEW.ID_COMMANDE;
             END$$            DELIMITER;
+
             DELIMITER        $$ CREATE TRIGGER `AFTER_UPDATE_LIGNE` AFTER UPDATE ON `LIGNE` FOR EACH ROW BEGIN DECLARE TOTALTTC DECIMAL(10, 2) DEFAULT 0;
             DECLARE          CONSOMMATIONTYPE INT DEFAULT 0;
             DECLARE          TVARATE DECIMAL(5, 3) DEFAULT 1.000;
@@ -50,17 +49,19 @@ BEGIN
                 -- Mettre à jour le total de la commande
                 UPDATE           COMMANDE SET TOTAL_COMMANDE = TOTALTTC WHERE COMMANDE.ID_COMMANDE = NEW.ID_COMMANDE;
                 END$$            DELIMITER;
+
+
                 DELIMITER        $$ CREATE TRIGGER `BEFORE_INSERT_LIGNE` BEFORE INSERT ON `LIGNE` FOR EACH ROW BEGIN DECLARE PRIXPRODUITHT DECIMAL(
                     10,
-                    2
-                ) DEFAULT 0;
- 
+                    2   ) DEFAULT 0; 
                 -- Obtenir le prix HT du produit
                 SELECT           PRIX_HT INTO PRIXPRODUITHT FROM PRODUIT WHERE PRODUIT.ID_PRODUIT = NEW.ID_PRODUIT;
  
                 -- Calculer le total HT de la ligne
                 SET              NEW.TOTAL_LIGNE_HT = PRIXPRODUITHT * NEW.QTE;
                 END$$            DELIMITER;
+
+
                 DELIMITER        $$ CREATE TRIGGER `BEFORE_UPDATE_LIGNE` BEFORE UPDATE ON `LIGNE` FOR EACH ROW BEGIN DECLARE PRIXPRODUITHT DECIMAL(10, 2) DEFAULT 0;
  
                 -- Obtenir le prix HT du produit
