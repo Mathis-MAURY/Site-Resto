e2a2ddb87963b71f4a2c888a61751833b1975fc6
MAURY Mathis <mathismaury31@gmail.com>
2025-03-19 11:17:10 +0100

    fix bug base de donnée sur le champ date

--- Site-Restaurant/inscription.php
+++ Site-Restaurant/inscription.php
@@ -1,7 +1,7 @@
 <?php
-  $pageTitle = "Inscription";
-  include 'fonctions/header.php';
-  include 'fonctions/ConnexionBDD.php';
+$pageTitle = "Inscription";
+include 'fonctions/header.php';
+include 'fonctions/ConnexionBDD.php';
 ?>
 
 <!DOCTYPE html>
@@ -21,69 +21,69 @@
         <p class="welcome">
             Remplissez les champs ci-dessous pour créer votre compte <span style="color: #00A45B;">FlashMeal </span> !
         </p>
-       
+
         <p class="separator"></p>
 
         <label> Adresse e-mail </label>
         <input type="email" id="email" name="email" required placeholder="exemple@domaine.com">
-        
+
         <p class="separator"></p>
 
         <label>Login</label>
         <input type="text" id="login" name="login" required placeholder="Votre identifiant">
 
         <p class="separator"></p>
-        
+
         <label>Mot de passe</label>
         <input type="password" id="password" name="password" required placeholder="************">
-        
+
         <a href="connexion.php" class="sublink">Vous avez déjà un compte ?</a>
-        
+
         <p class="separator"></p>
-        
-            <input type="submit" name="submit" value="Inscription">
+
+        <input type="submit" name="submit" value="Inscription">
     </form>
-    <?php 
-    
-// Créer une instance de la classe ConnexionBDD
-$db = new ConnexionBDD();
+    <?php
 
-// Établir la connexion à la base de données
-$pdo = $db->connect();
+    // Créer une instance de la classe ConnexionBDD
+    $db = new ConnexionBDD();
 
-// Vérifier si le formulaire a été soumis
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Récupérer les données du formulaire
-    $email = $_POST['email'] ?? null; // Utiliser l'opérateur null coalescent
-    $login = $_POST['login'] ?? null;
-    $password = $_POST['password'] ?? null;
+    // Établir la connexion à la base de données
+    $pdo = $db->connect();
 
-    // Vérifier que les champs ne sont pas vides
-    if (!empty($email) && !empty($login) && !empty($password)) {
-        // Hachage du mot de passe
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    // Vérifier si le formulaire a été soumis
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        // Récupérer les données du formulaire
+        $email = $_POST['email'] ?? null; // Utiliser l'opérateur null coalescent
+        $login = $_POST['login'] ?? null;
+        $password = $_POST['password'] ?? null;
 
-        try {
-            // Préparer la requête d'insertion
-            $stmt = $pdo->prepare('INSERT INTO user (email, login, password) VALUES (:email, :login, :password)');
+        // Vérifier que les champs ne sont pas vides
+        if (!empty($email) && !empty($login) && !empty($password)) {
+            // Hachage du mot de passe
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-            // Lier les paramètres aux valeurs
-            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-            $stmt->bindParam(':login', $login, PDO::PARAM_STR);
-            $stmt->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+            try {
+                // Préparer la requête d'insertion
+                $stmt = $pdo->prepare('INSERT INTO user (email, login, password) VALUES (:email, :login, :password)');
 
-            // Exécuter la requête
-            $stmt->execute();
+                // Lier les paramètres aux valeurs
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                $stmt->bindParam(':login', $login, PDO::PARAM_STR);
+                $stmt->bindParam(':password', $hashed_password, PDO::PARAM_STR);
 
-            echo "Inscription réussie !";
-        } catch (PDOException $e) {
-            echo 'Erreur lors de l\'inscription : ' . $e->getMessage();
+                // Exécuter la requête
+                $stmt->execute();
+
+                echo "Inscription réussie !";
+            } catch (PDOException $e) {
+                echo 'Erreur lors de l\'inscription : ' . $e->getMessage();
+            }
+        } else {
+            echo 'Veuillez remplir tous les champs.';
         }
-    } else {
-        echo 'Veuillez remplir tous les champs.';
     }
-}
-?>
+    ?>
 </body>
 
 </html>
--- Site-Restaurant/sql-exports/db_restoweb.sql
+++ Site-Restaurant/sql-exports/db_restoweb.sql
@@ -1,200 +1,212 @@
--- phpMyAdmin SQL Dump
+-- phpmyadmin sql dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : dim. 03 nov. 2024 à 17:18
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+-- hôte : 127.0.0.1
+-- généré le : dim. 03 nov. 2024 à 17:18
+-- version du serveur : 10.4.32-mariadb
+-- version de php : 8.2.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+set sql_mode = "no_auto_value_on_zero";
 
+start transaction;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+set time_zone = "+00:00";
+
+/*!40101 set @old_character_set_client=@@character_set_client */;
+
+/*!40101 set @old_character_set_results=@@character_set_results */;
+
+/*!40101 set @old_collation_connection=@@collation_connection */;
+
+/*!40101 set names utf8mb4 */;
 
 --
--- Base de données : `db_restoweb`
-CREATE DATABASE IF NOT EXISTS `DB_RESTOWEB` DEFAULT CHARACTER SET UTF8 COLLATE UTF8_GENERAL_CI;
+-- base de données : `db_restoweb`
+create database if not exists `db_restoweb` default character set utf8 collate utf8_general_ci;
 
-USE `DB_RESTOWEB`;
+use `db_restoweb`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `commande`
+-- structure de la table `commande`
 --
 
-CREATE TABLE `commande` (
-  `id_commande` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_etat` int(11) NOT NULL,
-  `date_commande` datetime NOT NULL DEFAULT current_timestamp(),
-  `total_commande` decimal(10,2) NOT NULL DEFAULT 0.00,
-  `type_conso` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+create table `commande` (
+  `id_commande` int(11) not null,
+  `id_user` int(11) not null,
+  `id_etat` int(11) not null,
+  `date` datetime not null default current_timestamp(),
+  `total_commande` decimal(10, 2) not null default 0.00,
+  `type_conso` tinyint(1) not null
+) engine=innodb default charset=utf8 collate=utf8_general_ci;
 
 --
--- Déchargement des données de la table `commande`
+-- déchargement des données de la table `commande`
 --
 
-INSERT INTO `commande` (`id_commande`, `id_user`, `id_etat`, `date_commande`, `total_commande`, `type_conso`) VALUES
-(16, 1, 0, '2024-10-29 14:38:05', 26.50, 1);
+insert into `commande` (
+  `id_commande`,
+  `id_user`,
+  `id_etat`,
+  `date`,
+  `total_commande`,
+  `type_conso`
+) values (
+  16,
+  1,
+  0,
+  '2024-10-29 14:38:05',
+  26.50,
+  1
+);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `ligne`
+-- structure de la table `ligne`
 --
 
-CREATE TABLE `ligne` (
-  `id_ligne` int(11) NOT NULL,
-  `id_commande` int(11) NOT NULL,
-  `id_produit` int(11) NOT NULL,
-  `qte` int(11) NOT NULL DEFAULT 0,
-  `total_ligne_ht` decimal(10,2) NOT NULL DEFAULT 0.00
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+create table `ligne` (
+  `id_ligne` int(11) not null,
+  `id_commande` int(11) not null,
+  `id_produit` int(11) not null,
+  `qte` int(11) not null default 0,
+  `total_ligne_ht` decimal(10, 2) not null default 0.00
+) engine=innodb default charset=utf8 collate=utf8_general_ci;
 
 --
--- Déchargement des données de la table `ligne`
+-- déchargement des données de la table `ligne`
 --
 
-INSERT INTO `ligne` (`id_ligne`, `id_commande`, `id_produit`, `qte`, `total_ligne_ht`) VALUES
-(26, 16, 2, 1, 14.00);
-
+insert into `ligne` (
+  `id_ligne`,
+  `id_commande`,
+  `id_produit`,
+  `qte`,
+  `total_ligne_ht`
+) values (
+  26,
+  16,
+  2,
+  1,
+  14.00
+);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `produit`
+-- structure de la table `produit`
 --
 
-CREATE TABLE `produit` (
-  `id_produit` int(11) NOT NULL,
-  `libelle` varchar(255) NOT NULL,
-  `prix_ht` decimal(10,2) NOT NULL,
-  `imageurl` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+create table `produit` (
+  `id_produit` int(11) not null,
+  `libelle` varchar(255) not null,
+  `prix_ht` decimal(10, 2) not null,
+  `imageurl` varchar(250) not null
+) engine=innodb default charset=utf8 collate=utf8_general_ci;
 
 --
--- Déchargement des données de la table `produit`
+-- déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id_produit`, `libelle`, `prix_ht`, `imageurl`) VALUES
+insert into `produit` (
+  `id_produit`,
+  `libelle`,
+  `prix_ht`,
+  `imageurl`
+) values
 (1, 'pizza margherita', 12.50, 'https://img.passeportsante.net/1200x675/2022-09-23/shutterstock-2105210927.webp'),
-(2, 'pizza chorizo', 14.00, 'https://www.galbani.fr/wp-content/uploads/2017/07/Image7.jpg'),
+(2, 'pizza chorizo', 14.00, 'https://www.galbani.fr/wp-content/uploads/2017/07/image7.jpg'),
 (3, 'assiette de charcuterie', 12.50, 'https://www.passionculinaire.fr/wp-content/uploads/2021/09/charcuterie-conseils.jpg'),
 (4, 'assiette de fromages', 10.50, 'https://cache.marieclaire.fr/data/photo/w999_c17/cuisine/43/fromages1.jpg'),
 (5, 'hamburger viande', 7.50, 'https://www.la-viande.fr/sites/default/files/inline-images/hamburger.jpg'),
 (6, 'hot dog', 5.00, 'https://www.tastingtable.com/img/gallery/13-best-hot-dogs-in-america/l-intro-1660217636.jpg'),
-(7, 'empanadas poulet', 11.00, 'https://www.enviedebienmanger.fr/sites/default/files/styles/img_orig/public/2020-12/68_1.png?itok=lfzsdW9K'),
-(8, 'empanadas thon', 9.00, 'https://www.seb.fr/medias/?context=bWFzdGVyfHJvb3R8NDQ4ODh8aW1hZ2UvanBlZ3xoM2YvaDI0LzE2NDgwMTYzMzk3NjYyLmpwZ3wzYTRjYWI3OTFkYjE5ZGFhODQ4MGZkMThiNDI3Njc3MTQ3MWE3OTkwYTc3MTYwYjc1YTI4ZGU5NjgyZTAzZGU4'),
+(7, 'empanadas poulet', 11.00, 'https://www.enviedebienmanger.fr/sites/default/files/styles/img_orig/public/2020-12/68_1.png?itok=lfzsdw9k'),
+(8, 'empanadas thon', 9.00, 'https://www.seb.fr/medias/?context=bwfzdgvyrhjvb3q4ndg4ohxpbwfnzs9qcgxobmvn'),
 (9, 'portion de frites', 5.00, 'https://brigade-hocare.com/info/wp-content/uploads/2022/10/portion-de-frites.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- structure de la table `user`
 --
 
-CREATE TABLE `utilisateur` (
-  `id_user` int(11) NOT NULL,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+create table `user` (
+  `id_user` int(11) not null,
+  `login` varchar(255) not null,
+  `password` varchar(255) not null,
+  `email` varchar(255) not null
+) engine=innodb default charset=utf8 collate=utf8_general_ci;
 
 --
--- Déchargement des données de la table `user`
+-- déchargement des données de la table `user`
 --
 
-INSERT INTO `utilisateur` (`id_user`, `login`, `password`, `email`) VALUES
-(1, 'leo', '$2y$10$OPEElGC0F4PghGraYRFl7OmH57cnVWvApKYF/oWDRmEkj8PVhiwBu', 'leo@m2l.fr'),
-(8, 'test', '$2y$10$wgYsVsexeJHE0Bitxz2OXuzvyGZEhEZdYoR/cxS02SnRkkCmLrxxa', 'test@test.fr'),
-(10, 'franck', '$2y$10$ZjmEtsUSjb4S4Z.OYC1XhOwDRoGd81LgxG0D/m4nNft5rh.HcAHTS', 'test2@test.fr');
+insert into `user` (
+  `id_user`,
+  `login`,
+  `password`,
+  `email`
+) values
+(1, 'leo', '$2y$10$opeelgc0f4pghgrayrfl7omh57cnvwvapkyf/owdrmekj8pvhiwbu', 'leo@m2l.fr'),
+(8, 'test', '$2y$10$wgysvsexejhe0bitxz2oxuzvygzehezdyor/cxs02snrkkcmlrxxa', 'test@test.fr'),
+(10, 'franck', '$2y$10$zjmetsusjb4s4z.oyc1xh0wdr0gd81lgxg0d/m4nnft5rh.hcahts', 'test2@test.fr');
 
---
--- Index pour les tables déchargées
---
+-- --------------------------------------------------------
 
---
--- Index pour la table `commande`
---
-ALTER TABLE `commande`
-  ADD PRIMARY KEY (`id_commande`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_etat` (`id_etat`);
+-- index pour les tables déchargées
 
---
--- Index pour la table `ligne`
---
-ALTER TABLE `ligne`
-  ADD PRIMARY KEY (`id_ligne`),
-  ADD KEY `id_commande` (`id_commande`),
-  ADD KEY `id_produit` (`id_produit`);
+-- index pour la table `commande`
+alter table `commande`
+  add primary key (`id_commande`),
+  add key `id_user` (`id_user`),
+  add key `id_etat` (`id_etat`);
 
---
--- Index pour la table `produit`
---
-ALTER TABLE `produit`
-  ADD PRIMARY KEY (`id_produit`);
+-- index pour la table `ligne`
+alter table `ligne`
+  add primary key (`id_ligne`),
+  add key `id_commande` (`id_commande`),
+  add key `id_produit` (`id_produit`);
 
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+-- index pour la table `produit`
+alter table `produit`
+  add primary key (`id_produit`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
+-- index pour la table `user`
+alter table `user`
+  add primary key (`id_user`);
 
---
--- AUTO_INCREMENT pour la table `commande`
---
-ALTER TABLE `commande`
-  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+-- auto_increment pour les tables déchargées
 
---
--- AUTO_INCREMENT pour la table `ligne`
---
-ALTER TABLE `ligne`
-  MODIFY `id_ligne` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+-- auto_increment pour la table `commande`
+alter table `commande` modify `id_commande` int(11) not null auto_increment, auto_increment=22;
 
---
--- AUTO_INCREMENT pour la table `produit`
---
-ALTER TABLE `produit`
-  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+-- auto_increment pour la table `ligne`
+alter table `ligne` modify `id_ligne` int(11) not null auto_increment, auto_increment=38;
 
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+-- auto_increment pour la table `produit`
+alter table `produit` modify `id_produit` int(11) not null auto_increment, auto_increment=10;
 
---
--- Contraintes pour les tables déchargées
---
+-- auto_increment pour la table `user`
+alter table `user` modify `id_user` int(11) not null auto_increment, auto_increment=11;
 
---
--- Contraintes pour la table `commande`
---
-ALTER TABLE `commande`
-  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+-- contraintes pour les tables déchargées
 
---
--- Contraintes pour la table `ligne`
---
-ALTER TABLE `ligne`
-  ADD CONSTRAINT `ligne_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id_commande`),
-  ADD CONSTRAINT `ligne_ibfk_2` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id_produit`);
-COMMIT;
+-- contraintes pour la table `commande`
+alter table `commande`
+  add constraint `commande_ibfk_1` foreign key (`id_user`) references `user` (`id_user`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- contraintes pour la table `ligne`
+alter table `ligne`
+  add constraint `ligne_ibfk_1` foreign key (`id_commande`) references `commande` (`id_commande`),
+  add constraint `ligne_ibfk_2` foreign key (`id_produit`) references `produit` (`id_produit`);
+
+commit;
+
+/*!40101 set character_set_client=@old_character_set_client */;
+
+/*!40101 set character_set_results=@old_character_set_results */;
+
+/*!40101 set collation_connection=@old_collation_connection */;
